c36eda2344ed481b995d9349d571e1fed0160744
Bruno <brunomircevski@protonmail.com>
2023-04-23 16:31:21 +0200

    Początek pracy nad front end app.html

--- src/renderer/js/app.js
+++ src/renderer/js/app.js
@@ -6,8 +6,36 @@ const store = new Store();
 const url = store.get('serverAddress');
 const username = store.get('username');
 
-const jwtBox = document.getElementById("jwt");
+const SendMessageInput = document.getElementById("message-input");
+const sendMessageBtn = document.getElementById("message-send");
+const sendMessageForm = document.getElementById("message-form");
 
-window.addEventListener('load', () => {
-    jwtBox.innerText = window.sessionStorage.getItem('jwt');
+
+window.addEventListener("load", () => {
+    //jwtBox.innerText = window.sessionStorage.getItem('jwt');
+});
+
+SendMessageInput.addEventListener("input", () => {
+    SendMessageInput.style.height = "1px";
+    let h = 2 + SendMessageInput.scrollHeight;
+    if (h > 200) h = 200;
+    SendMessageInput.style.height = h + "px";
+});
+
+SendMessageInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+        event.preventDefault();
+        sendMessageBtn.click();
+    }
+});
+
+sendMessageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const message = SendMessageInput.value.trim();
+
+    if(message == "") return;
+
+    console.log(message);
+    SendMessageInput.style.height = "36px";
+    SendMessageInput.value = "";
 });
